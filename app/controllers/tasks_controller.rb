43d35a1d82499a6ae5529df6b2c03f4370bf9b18
Vinicius Alves Beló <vinibelo@MacBook-Pro-de-Vinicius.local>
2024-10-14 19:11:28 -0300

    Destroy endpoint added for tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,6 @@
 class TasksController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_task, only: %i[destroy]
 
   def index
     render json: {
@@ -8,15 +9,29 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(context_id: params[:context_id], **permit_params)
+    @task = Task.new(context_id: params[:context_id], **task_params)
     return render unprocessable_entity_response unless @task.save!
     render json: @task, status: :created
   end
 
+  def destroy
+    return unprocessable_entity_response unless @task.destroy!
+    render json: { message: "Task deleted successfully" }, status: :no_content
+  end
+
   private
 
-  def permit_params
+  def task_params
     params.require(:task).permit(:title, :description, :start_date, :end_date)
+  end
+
+  def set_task
+    @task = Task.find_by(id: params[:id], context_id: params[:context_id])
+    not_found_response if @task.blank?
+  end
+
+  def not_found_response
+    render json: { message: "Not found task with given id" }, status: :not_found
   end
 
   def unprocessable_entity_response
