11b45472f042634ad64e2569b1bd5fe137553bd5
Vinicius Alves Beló <vinibelo@MacBook-Pro-de-Vinicius.local>
2024-10-15 20:08:07 -0300

    Refactored context jsons

--- app/controllers/contexts_controller.rb
+++ app/controllers/contexts_controller.rb
@@ -4,28 +4,43 @@ class ContextsController < ApplicationController
 
   def index
     render json: {
-      contexts: Context.where(user_id: current_user.id)
+      method: "#{controller_name}##{action_name}",
+      contexts: Context.where(user_id: current_user.id).map do |context|
+        serialize_context(context)
+      end
     }, status: :ok
   end
 
   def show
-    render json: @context, status: :ok
+    render json: {
+      method: "#{controller_name}##{action_name}",
+      context: serialize_context(@context)
+    }, status: :ok
   end
 
   def create
     @context = Context.new(user_id: current_user.id, **context_params)
     return unprocessable_entity_response unless @context.save
-    render json: @context, status: :created
+    render json: {
+      method: "#{controller_name}##{action_name}",
+      context: serialize_context(@context)
+    }, status: :created
   end
 
   def update
     return unprocessable_entity_response unless @context.update(context_params)
-    render json: @context, status: :ok
+    render json: {
+      method: "#{controller_name}##{action_name}",
+      context: serialize_context(@context)
+    }, status: :ok
   end
 
   def destroy
     return unprocessable_entity_response unless @context.destroy!
-    render json: { message: "Context deleted successfully" }, status: :no_content
+    render json: {
+      method: "#{controller_name}##{action_name}",
+      message: "Context deleted successfully"
+    }, status: :no_content
   end
 
   private
@@ -34,16 +49,26 @@ class ContextsController < ApplicationController
     params.require(:context).permit(:title, :description)
   end
 
+  def serialize_context(context)
+    ContextSerializer.new(context).serializable_hash[:data][:attributes]
+  end
+
   def set_context
     @context = Context.find_by(id: params[:id])
     not_found_response if @context.blank?
   end
 
   def not_found_response
-    render json: { message: "Not found context with given id" }, status: :not_found
+    render json: {
+      method: "#{controller_name}##{action_name}",
+      message: "Not found context with given id"
+    }, status: :not_found
   end
 
   def unprocessable_entity_response
-    render json: @context, status: :unprocessable_entity
+    render json: {
+      method: "#{controller_name}##{action_name}",
+      context: @context
+    }, status: :unprocessable_entity
   end
 end
