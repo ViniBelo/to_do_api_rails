a0ee1134ac93efcfc1d7fc3ae15b5189a6902200
Vinicius Alves Beló <vinibelo@MacBook-Pro-de-Vinicius.local>
2024-10-22 21:01:51 -0300

    Refactore user_serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,8 @@
 class UserSerializer
   include JSONAPI::Serializer
-  attribute :email, :user_details
+  attribute :email
+
+  attribute :user_details do |user|
+    user.user_details.to_json
+  end
 end
