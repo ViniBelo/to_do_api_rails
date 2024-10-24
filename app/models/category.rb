b21da14aac741668803b5c8b3e18f36e2b6ede5d
Vinicius Alves Beló <vinibelo@MacBook-Pro-de-Vinicius.local>
2024-10-24 19:33:43 -0300

    Fixed task_categories relation on category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,7 @@
 class Category < ApplicationRecord
   belongs_to :context
   has_many :task_categories, dependent: :destroy
-  has_many :categories, through: :task_categories
+  has_many :tasks, through: :task_categories
 
   def to_json
     {
